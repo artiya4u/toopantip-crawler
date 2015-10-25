7f792952f13a6666c896472fb9a6930011c55489
Artiya T <artiya4u@gmail.com>
2015-10-26 02:38:56 +0700

    Fix script not run in cron.

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
+
+# Export path for scrapy.
+PATH=$PATH:/usr/local/bin
+export PATH
+
+# Run crawler.
 scrapy runspider pantip_spider.py -o topics-temp.json
+
+# Move temp file to real file.
 mv -f topics-temp.json topics.json
