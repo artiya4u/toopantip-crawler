05b17d5c56d966561869e71a1d3222828437d06f
Artiya T <artiya4u@gmail.com>
2015-10-26 12:22:14 +0700

    Fix  parsing error.

--- pantip_spider.py
+++ pantip_spider.py
@@ -51,11 +51,3 @@ class TopicItem(scrapy.Item):
 
     category = scrapy.Field()
 
-
-process = CrawlerProcess({
-    'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
-})
-
-process.crawl(PantipSpider)
-process.start()
-
