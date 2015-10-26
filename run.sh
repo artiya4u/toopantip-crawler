b8ddf82df5a6f12eb5371f5518152b7dd00b8754
Artiya T <artiya4u@gmail.com>
2015-10-26 09:25:03 +0700

    Fix run script to prevent crawler error.

--- run.sh
+++ run.sh
@@ -4,8 +4,18 @@
 PATH=$PATH:/usr/local/bin
 export PATH
 
-# Run crawler.
-scrapy runspider pantip_spider.py -o topics-temp.json
+file=topics-temp.json
+minimumsize=200
 
-# Move temp file to real file.
-mv -f topics-temp.json topics.json
+while true; do
+    # Run crawler.
+    scrapy runspider pantip_spider.py -o $file
+    actualsize=$(wc -c <"$file")
+    if [ $actualsize -ge $minimumsize ]; then
+        # Move temp file to real file.
+        mv -f $file topics.json
+        break
+    fi
+done
+
+
