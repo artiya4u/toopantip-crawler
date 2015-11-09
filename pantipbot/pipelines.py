ea4e073c5c3e12c9fe8497421ad77dc27cfd855c
Artiya T <artiya4u@gmail.com>
2015-11-09 10:17:31 +0700

    Add banned topic list to filter out bad content.

--- pantipbot/pipelines.py
+++ pantipbot/pipelines.py
@@ -8,10 +8,16 @@ class FilterExistTopicPipeline(object):
 
     # put exist topic.
     topic_ids = set()
+    banned_ids = list()
+
+    # Image is too scared.
+    banned_ids.append('34382712')
 
     def process_item(self, item, spider):
         if item['id'] in self.topic_ids:
             raise DropItem("Already have topic: %s" % item['id'])
+        elif item['id'] in self.banned_ids:
+            raise DropItem("Banned topic: %s" % item['id'])
         else:
             self.topic_ids.add(item['id'])
             return item
