2172a4306350bc44c43709657676b8c93240450f
Artiya T <artiya4u@gmail.com>
2015-12-08 01:26:45 +0700

    Not sort result to keep content variety.

--- run.sh
+++ run.sh
@@ -5,12 +5,9 @@ PATH=$PATH:/usr/local/bin
 export PATH
 
 file=topics-temp.json
-unsorted_file=topics-unsorted.json
 minimum_size=2000
 rm ${file}
-rm ${unsorted_file}
-scrapy crawl pantipspider -o ${unsorted_file} --logfile pantip_spider.log
-python sort_topic_json.py
+scrapy crawl pantipspider -o ${file} --logfile pantip_spider.log
 actual_size=$(wc -c <"$file")
 if [ ${actual_size} -ge ${minimum_size} ]; then
     # Move temp file to real file.
