33ab5345bb76e296fa2530382c6f4f1938201d2e
Artiya Thinkumpang <artiya@siamsquared.co>
2017-08-03 13:04:50 +0700

    Change upload path

--- run.sh
+++ run.sh
@@ -15,7 +15,7 @@ actual_size=$(wc -c <"$file")
 if [ ${actual_size} -ge ${minimum_size} ]; then
     # Move temp file to real file.
     mv -f ${file} topics.json
-    sftp robot@artiya.me:toopantip-crawler <<< $'put topics.json'
+    sftp robot@artiya.me <<< $'put topics.json'
 fi
 
 
